aaafa1cd3e83cd7a4c18712a10a76886361f7b01
Andabeat <romina@nexiadesign.com>
2025-10-21 14:00:55 -0400

    Add Landing_Page component and convert files to JSX

--- src/App.js
+++ src/App.js
@@ -7,22 +7,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Import custom Navbar component
 import Navbar from './Components/Navbar/Navbar';
 
+// Import Landing_Page component
+import Landing_Page from './Components/Landing_Page/Landing_Page';
+
 // Function component for the main App
 function App() {
 
   // Render the main App component
   return (
     <div className="App">
-        {/* Set up BrowserRouter for routing */}
-        <BrowserRouter>
-          {/* Display the Navbar component */}
-          <Navbar/>
+      {/* Set up BrowserRouter for routing */}
+      <BrowserRouter>
+        {/* Display the Navbar component */}
+        <Navbar />
 
-          {/* Set up the Routes for different pages */}
-          <Routes>
-            {/* Define individual Route components for different pages */}
-          </Routes>
-        </BrowserRouter>
+        {/* Set up the Routes for different pages */}
+        <Routes>
+          {/* Define individual Route components for different pages */}
+          <Route path="/" element={<Landing_Page />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
